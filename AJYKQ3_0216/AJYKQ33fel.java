fd29dc3cce896cefdb12adbb7b3ea69718fe79ed
GMárton <martonstar@gmail.com>
2022-02-19 13:42:53 +0100

    3-as feladat végelegesítése

--- AJYKQ3_0216/AJYKQ33fel.java
+++ AJYKQ3_0216/AJYKQ33fel.java
@@ -2,6 +2,17 @@ package gyak;
 
 public class AJYKQ33fel {
 
-	
-	
+	public void Connect() {
+		Connection conn = null;
+		String url = "jdbc:oracle:thin:@193.6.5.58:1521:XE";
+		String user = "username";
+		String pwd = "password";
+		
+		try {
+			conn = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Sikeres kapcsolódás\n");
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 }
