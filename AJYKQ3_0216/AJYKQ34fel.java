446e66350f91a811577c5a44f6dcbd08576c8cb4
GMárton <martonstar@gmail.com>
2022-02-19 13:46:46 +0100

    4-es feladat véglegesítése

---
+++ AJYKQ3_0216/AJYKQ34fel.java
@@ -0,0 +1,15 @@
+package gyak;
+
+public class AJYKQ34fel {
+
+	public void LeKapcs() {
+  if (conn != null) {
+  try {
+      conn.close();
+      System.out.println("Sikeres lekapcsolódás!\n");
+      } catch (Exception ex) {
+      System.err.println(ex.getMessage());
+      }
+    }
+  }
+}
