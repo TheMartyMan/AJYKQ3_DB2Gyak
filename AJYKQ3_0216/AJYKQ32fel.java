2fa251afa5f1c1701ce924f9fe6ece2091e73cc8
GMárton <martonstar@gmail.com>
2022-02-19 15:13:00 +0100

    2-es feladat véglegesítése

--- AJYKQ3_0216/AJYKQ32fel.java
+++ AJYKQ3_0216/AJYKQ32fel.java
@@ -5,7 +5,8 @@ public class AJYKQ32fel {
 	public void DriverReg() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("Sikeres driver regisztr·l·s!\n");
+			System.out.println("Sikeres driver regisztr√°l√°s!\n");
+			
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
