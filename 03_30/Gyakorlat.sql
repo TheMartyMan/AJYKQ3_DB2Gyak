c91df7b491732fc3ea5c46cd71abccf6c2ee224b
GMárton <martonstar@gmail.com>
2022-03-30 11:48:42 +0200

    Update Gyakorlat.sql

--- 03_30/Gyakorlat.sql
+++ 03_30/Gyakorlat.sql
@@ -1,1 +1,30 @@
+---8. gyakorlat---
+---Feladatok---
+
+DROP TABLE autok;
+---1. feladat
+/*
+CREATE TABLE autok (
+rsz int PRIMARY KEY NOT NULL,
+tipus char,
+kor int,
+ar int,
+szin char
+);
+*/
+
+---3. feladat
+/*
+insert into auto values ('ABC500', 'Opel Corsa', 'Piros', 8, 720000);
+insert into auto values ('MCC325', 'Opel Insignia', 'Fekete', 2, 6800000);
+insert into auto values ('BBM104', 'Suzuki Swift', 'Piros', 5, 1500000);
+insert into auto values ('CHR411', 'Renault Twingo', 'Piros', 12, 630000);
+insert into auto values ('JRZ932', 'BMW M3', 'Fekete', 5, 4500000);
+insert into auto values ('JEG113', 'Opel Corsa', 'Piros', 7, 810000);
+insert into auto values ('DER842', 'Seat Ibiza', 'Szürke', 14, 4500000);
+insert into auto values ('BAB422', 'Lada 1300S', 'Fehér', 28, 1980000);
+insert into auto values ('UFF666', 'Audi TT', 'Fekete', 4, 7500000);
+insert into auto values ('ABC124', 'Fiat', 'Piros', 3, 18000000);
+/*
+
 
