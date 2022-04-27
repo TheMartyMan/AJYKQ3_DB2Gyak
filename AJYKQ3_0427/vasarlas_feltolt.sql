5f990fd4113a7f2ecc208280ee70ce2bfefca3d7
GMárton <martonstar@gmail.com>
2022-04-27 10:52:49 +0200

    Create vasarlas_feltolt.sql

---
+++ AJYKQ3_0427/vasarlas_feltolt.sql
@@ -0,0 +1,11 @@
+begin
+insert into vasarlas values (1, sysdate, 't14', 1, 'v06');
+insert into vasarlas values (2, sysdate, 't15', 1, 'v06');
+insert into vasarlas values (4, sysdate, 't03', 4, 'v01');
+insert into vasarlas values (5, sysdate, 't04', 12, 'v01');
+insert into vasarlas values (6, sysdate, 't02', 2, 'v02');
+insert into vasarlas values (7, sysdate, 't06', 2, 'v02');
+insert into vasarlas values (8, sysdate, 't07', 4, 'v02');
+insert into vasarlas values (11, sysdate, 't11', 3, 'v04');
+insert into vasarlas values (13, sysdate, 't13', 100, 'v05');
+end;
