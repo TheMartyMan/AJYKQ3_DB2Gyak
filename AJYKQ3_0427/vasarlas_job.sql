a61cfff2cce1b04369fe9f6e5058efbada14b856
GMárton <martonstar@gmail.com>
2022-04-27 11:19:33 +0200

    Create vasarlas_job.sql

---
+++ AJYKQ3_0427/vasarlas_job.sql
@@ -0,0 +1,10 @@
+BEGIN
+DBMS_SCHEDULER.CREATE_JOB (
+ job_name => 'vasarlas_job',
+ job_type => 'STORED_PROCEDURE',
+ job_action => 'VDbKiir',
+ start_date => SYSTIMESTAMP,
+ enabled => true,
+ repeat_interval => 'FREQ=MINUTELY; INTERVAL=1',
+ comments => 'Vasarlas db');
+END;
